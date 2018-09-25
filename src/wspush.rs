135fd8115813d00bbc45d0edbda1b006ff9694c7
Uwe Klotz <uwe_klotz@web.de>
2018-09-25 16:22:03 +0200

    Create WS connection filters

--- src/wspush.rs
+++ src/wspush.rs
@@ -4,10 +4,16 @@
 ////////////////////////////////////////////////////////////////////////
 use super::*;
 
-use futures::{stream, Sink};
+use futures::{stream, Future, Sink};
 
-use warp::ws::{Message as WarpMessage, WebSocket};
+use std::sync::Arc;
 
+use warp::{
+    ws::{Message as WarpMessage, WebSocket},
+    Filter,
+};
+
+use slowtec_core::communication::connection::*;
 use slowtec_core::communication::message::MessagePayload;
 use slowtec_core::communication::push::PushConnection;
 
@@ -44,4 +50,32 @@ impl PushConnection for WsPushConnection {
             .map(|_| ())
             .map_err(Into::into)
     }
+}
+
+pub trait WsConnectionContext: Send {
+    fn handle_connection(
+        self,
+        ws: WebSocket,
+        connection_id: ConnectionId,
+    ) -> Box<dyn Future<Item = (), Error = ()> + Send>;
+}
+
+pub fn new_connection_filter<N, C>(
+    path: &'static str,
+    new_connection_context: N,
+) -> warp::filters::BoxedFilter<(impl warp::Reply,)>
+where
+    N: NewConnectionContext<Instance = C> + Send + Sync + 'static,
+    C: WsConnectionContext + 'static,
+{
+    let id_generator = Arc::new(ConnectionIdGenerator::default());
+    let id_filter = warp::any().map(move || id_generator.generate_id());
+    let context_filter = warp::any().map(move || new_connection_context.new_connection_context());
+    warp::path(path)
+        .and(warp::ws2())
+        .and(id_filter)
+        .and(context_filter)
+        .map(|ws2: warp::ws::Ws2, connection_id, connection_context: C| {
+            ws2.on_upgrade(move |ws| connection_context.handle_connection(ws, connection_id))
+        }).boxed()
 }
